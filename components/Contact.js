ce6a3ba19db10e4162e0f28dd23ca801fc96fb94
Krzysztof Gertig <ktgertig@gmail.com>
2018-03-28 17:24:17 +0100

    Project

---
+++ components/Contact.js
@@ -0,0 +1,12 @@
+<div className={'contactItem'}>
+    <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
+    <p className={'contactLabel'}>
+        Imię: {this.props.contact.firstName}
+    </p>
+    <p className={'contactLabel'}>
+        Nazwisko: {this.props.contact.lastName}
+    </p>
+    <a href={'mailto:' + this.props.item.email}>
+        {this.props.item.email}
+    </a>
+</div>
