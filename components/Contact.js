7eb297eecae72445551cadaf5879cfac2f38426b
ktgertig <35666978+ktgertig@users.noreply.github.com>
2018-03-28 18:41:20 +0100

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -1,9 +1,22 @@
-<div className={'contactItem'}>
-    <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
-    <p className={'contactLabel'}>
-        Imię: {this.props.contact.firstName}
-    </p>
-    <a href={'mailto:' + this.props.item.email}>
-        {this.props.item.email}
-    </a>
-</div>
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+    	<div className={'contactItem'}>
+			<img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
+			<p className={'contactLabel'}>
+        		Imie: {this.props.item.firstName}
+    		</p>
+  			<p className={'contactLabel'}>
+        		Nazwisko: {this.props.item.lastName}
+    		</p>
+    		<a href={'mailto:' + this.props.item.email}>
+        		{this.props.item.email}
+    		</a>
+		</div>
+    )
+  },
+});
